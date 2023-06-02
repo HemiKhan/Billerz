806bf53e08406f7cca404d25d3befdec3031f877
muhammad.hammas <muhammad.hammas@DEVELOPER-13>
2023-06-02 14:56:39 +0500

    .

---
+++ Billerz/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Billerz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
