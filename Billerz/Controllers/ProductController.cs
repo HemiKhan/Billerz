879736627ca386dad4b3b370b949168f6e2b423d
Hemi Khan <114137475+HemiKhan@users.noreply.github.com>
2023-06-02 10:21:26 -0700

    .

---
+++ Billerz/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Billerz.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetail()
+        {
+            return View();
+        }
+    }
+}
