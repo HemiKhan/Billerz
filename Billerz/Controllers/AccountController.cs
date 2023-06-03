c9bec9281f4a401a4ccef283db1b18bd1ead0c07
ahsan7266 <74853314+ahsan7266@users.noreply.github.com>
2023-06-03 19:04:32 +0500

    done

---
+++ Billerz/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Billerz.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult LoginRegister()
+        {
+            return View();
+        }
+        public IActionResult MyAccount()
+        {
+            return View();
+        }
+    }
+}
---
+++ Billerz/Controllers/PagesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Billerz.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Faq()
+        {
+            return View();
+        }
+        public IActionResult Privacypolicy()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult TermsConditions()
+        {
+            return View();
+        }
+        public IActionResult SizeGuide()
+        {
+            return View();
+        }
+        public IActionResult ReturnPolicy()
+        {
+            return View();
+        }
+        public IActionResult ShippingInformation()
+        {
+            return View();
+        }
+        public IActionResult Help()
+        {
+            return View();
+        }
+        public IActionResult Covid19()
+        {
+            return View();
+        }
+    }
+}
